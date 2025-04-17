df3393b0a7fc6e44c491cd875606d86c2c44890f
Scott Gibb <smgibb@yahoo.com>
2025-04-17 22:55:55 +0100

    Adding CI

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# This script is used to run the LED-CMDline-Controller in a Docker container.
 docker build . -t cmdline-controller:v1
 docker run -d -it \
     --name LED-CMDline-Controller \
