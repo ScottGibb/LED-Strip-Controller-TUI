825f082667e5ddad3250d173dffe4f17736a016c
ScottGibb <44235263+ScottGibb@users.noreply.github.com>
2025-04-17 22:13:27 +0000

    [MegaLinter] Apply linters automatic fixes

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # This script is used to run the LED-CMDline-Controller in a Docker container.
 docker build . -t cmdline-controller:v1
 docker run -d -it \
-    --name LED-CMDline-Controller \
-    --privileged \
-    cmdline-controller:v1
+	--name LED-CMDline-Controller \
+	--privileged \
+	cmdline-controller:v1
